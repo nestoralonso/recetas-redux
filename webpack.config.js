dd3d906aa1eff358515f5d45c6043331e01321f6
NOM <n@n>
2016-07-04 10:34:12 -0500

    Added raw-loader and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,21 +7,26 @@ module.exports = {
     'react-hot-loader/patch',
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    './src/main'
+    './src/main',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/'
+    publicPath: '/dist/',
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+      include: path.join(__dirname, 'src'),
+    },
+    {
+      test: /\.css?$/,
+      loaders: ['style', 'raw'],
+      include: path.join(__dirname, 'css'),
+    }],
+  },
 };
