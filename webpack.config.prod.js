d141d0e6b1fbcbf948109fd8ac7a6e4857edd92e
NOM <n@n>
2016-07-07 15:39:01 -0500

    fixed linting issues nut still doesn't work :confused:

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,39 +1,39 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   devtool: 'source-map',
   entry: [
-    './src/main.js'
+    './src/main.js',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/'
+    publicPath: '/dist/',
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
-        warnings: false
-      }
-    })
+        warnings: false,
+      },
+    }),
   ],
   module: {
     loaders: [{
       test: /\.jsx?/,
       loaders: ['babel'],
-      include: path.join(__dirname, 'src')
+      include: path.join(__dirname, 'src'),
     },
     {
       test: /\.css?$/,
-      loaders: [ 'style', 'raw' ],
-      include: path.join(__dirname, 'css')
-    }]
-  }
+      loaders: ['style', 'raw'],
+      include: path.join(__dirname, 'css'),
+    }],
+  },
 };
